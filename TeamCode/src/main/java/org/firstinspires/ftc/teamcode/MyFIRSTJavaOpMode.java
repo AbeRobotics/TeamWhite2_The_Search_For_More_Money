e2cdd81d20494c4bca233524e4cc4db9f3bbb25e
ByeNow12 <neibourhoodnerd@gmail.com>
2018-01-23 22:44:13 -0700

    First iteration teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -4,89 +4,112 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by 500260501 on 13/11/2017.
- * Alexander Aldridge promptly attempted to improve it
+ * Alexander Aldridge promptly attempted to improve it;
+ * Riley James then attempted to further improve it
+ * Ibrahim Sultan was there for the heck of it!
  */
 @TeleOp(name="OpMode 2.1: The Journey for more Money", group="Teleop")
 //@Disabled
 public class MyFIRSTJavaOpMode extends OpMode {
     DcMotor leftWheel;
     DcMotor rightWheel;
+    DcMotor shoulder;
+    DcMotor elbow;
+
+    Servo leftClaw;
+    Servo rightClaw;
+    Servo littleArmThingFeaturingSkeet;
     //DcMotor liftMotor;
 
     double leftWheelPower;
     double rightWheelPower;
-
+    double shoulderPower;
+    double elbowPower;
+    double leftClawClosePosition = 0.49;
+    double leftClawOpenPosition = 0.75;
+    double rightClawClosePosition = 0.5;
+    double rightClawOpenPosition = 0.25;
+    double littleArmFeaturingSkeetDownPosition = 1;
+    double littleArmFeaturingSkeetUpPosition = 0.51;
+    boolean skeetDown = false;
+    int aTicks = 0;     //counts number of ticks the a button has been held down to prevent the littleArmThingFeaturingSkeet from shaking
     @Override
     public void init()
     {
-        leftWheel = hardwareMap.dcMotor.get("left_drive");
-        rightWheel = hardwareMap.dcMotor.get("right_drive");
-        //liftMotor = hardwareMap.dcMotor.get("lift_motor");
+        leftWheel = hardwareMap.dcMotor.get("left_wheel");
+        rightWheel = hardwareMap.dcMotor.get("right_wheel");
+        shoulder = hardwareMap.dcMotor.get("shoulder");
+        elbow = hardwareMap.dcMotor.get("elbow");
 
-        //leftClaw = hardwareMap.servo.get("left_claw");
-        //rightClaw = hardwareMap.servo.get("right_claw");
-        //leftArm = hardwareMap.servo.get("left_arm");
-        //rightArm = hardwareMap.servo.get("right_arm");
+        leftClaw = hardwareMap.servo.get("left_claw");
+        rightClaw = hardwareMap.servo.get("right_claw");
+        littleArmThingFeaturingSkeet = hardwareMap.servo.get("little_arm_thing");
 
-        //leftClaw.setPosition(0.5);
-        //rightClaw.setPosition(0.5);
+        //leftClawClosePosition = leftClaw.get
 
-        //leftArm.setPosition(leftArmDefaultPosition);
-        //rightArm.setPosition(rightArmDefaultPosition);
+        leftClaw.setPosition(0.5);
+        rightClaw.setPosition(0.5);
+        littleArmThingFeaturingSkeet.setPosition(littleArmFeaturingSkeetUpPosition);
 
-        rightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop()
     {
-        leftWheelPower = gamepad1.left_stick_y;
+        leftWheelPower = -gamepad1.left_stick_y;
         rightWheelPower = gamepad1.right_stick_y;
 
-        leftWheel.setPower(1);
-        rightWheel.setPower(1);
+        leftWheel.setPower(leftWheelPower);
+        rightWheel.setPower(rightWheelPower);
 
-        //Controlling the claw with the triggers
-       // if(gamepad1.left_trigger > 0.01)
-        //{
-        //}
-        //if(gamepad1.right_trigger > 0.01)
-        //{
-        //}
-
-        //Controlling the lift mechanism using y and a buttons
-        //if(gamepad1.y)
-        //{
-            //liftMotor.setPower(1.0);
-        //}
-        //else if(gamepad1.a)
-        //{
-            //liftMotor.setPower(-1.0);
-        //}
-        //else
-        //{
-            //liftMotor.setPower(0);
-        //}
-
-        //Controlling the arms
-        //if(gamepad1.x)
-        //{
-            //leftArm.setPosition(leftArmPosition);
-        //}
-        //else
-        //{
-            //leftArm.setPosition(leftArmDefaultPosition);
-        //}
-        /*if(gamepad1.b)
-        {
-            //rightArm.setPosition(rightArmPosition);
+        if (gamepad1.left_bumper){
+            leftClaw.setPosition(leftClawClosePosition);
+            rightClaw.setPosition(rightClawClosePosition);
         }
-        else
-        {
-            //rightArm.setPosition(rightArmDefaultPosition);
-        }*/
+
+        if (gamepad1.right_bumper){
+            leftClaw.setPosition(leftClawOpenPosition);
+            rightClaw.setPosition(rightClawOpenPosition);
+        }
+
+        if (gamepad1.a){
+            if (aTicks < 11) {
+                if (skeetDown) {
+                    skeetDown = false;
+                    littleArmThingFeaturingSkeet.setPosition(littleArmFeaturingSkeetUpPosition);
+                }
+                else {
+                    skeetDown = true;
+                    littleArmThingFeaturingSkeet.setPosition(littleArmFeaturingSkeetDownPosition);
+                }
+                aTicks++;
+            }
+        }
+        else{
+            aTicks = 0;
+        }
+
+        if (gamepad1.dpad_up){
+            shoulder.setPower(0.5);
+        }
+        else if (gamepad1.dpad_down){
+            shoulder.setPower(-0.5);
+        }
+        else{
+            shoulder.setPower(0);
+        }
+        if (gamepad1.dpad_left){
+            elbow.setPower(0.5);
+        }
+        else if (gamepad1.dpad_right){
+            elbow.setPower(-0.5);
+        }
+        else{
+            elbow.setPower(0);
+        }
     }
 }
